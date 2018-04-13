761aa77fe25070b9dba8c8aef97e803b0ba96ed6
bunny-girl <37472759+bunny-girl@users.noreply.github.com>
2018-04-13 14:25:37 +0800

    Update OS X Init.sh
    Update from echo to read.

--- OS X Init.sh
+++ OS X Init.sh
@@ -1,5 +1,44 @@
-echo "Install Oceanic Theme for OSX Terminal from https://github.com/robinbentley/oceanic-next-macos-terminal"
-cd ~/Download/
-mkdir _tmp
-git clone git@github.com:robinbentley/oceanic-next-macos-terminal.git
-echo "Cloned. Plz import into Preference of Terminal"
+read -p "Install Homebrew? y/n" hb
+case $hb in
+  "y")
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo "Homebrew installed."
+    ;;
+  "n")
+    echo "Homebrew installation cancelled."
+    ;;
+esac
+
+read -p "Install NodeJS? y/n" n
+case $n in
+  "y")
+    brew install nodejs
+    echo "NodeJS installed"
+    ;;
+  "n")
+    echo "NodeJS installation cancelled."
+    ;;
+esac
+
+read -p "Download Oceanic Theme for OSX Terminal from https://github.com/robinbentley/oceanic-next-macos-terminal ? y/n" ocean
+case $ocean in
+  "y")
+    cd ~/Download/
+    mkdir _tmp
+    git clone git@github.com:robinbentley/oceanic-next-macos-terminal.git
+    echo "Cloned. Plz import into Preference of Terminal"
+    ;;
+  "n")
+    echo "Download cancelled."
+    ;;
+esac
+
+read -p "Generate ssh keys? y/n" sshkey
+case $sshkey in
+  "y")
+    ssh-keygen -t rsa 
+    ;;
+  "n")
+    echo "SSH key generation cancelled"
+    ;;
+esac
