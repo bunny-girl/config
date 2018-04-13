261fa892b2b328f0f97b2dd77ca344661961243b
bunny-girl <37472759+bunny-girl@users.noreply.github.com>
2018-04-13 14:16:04 +0800

    Update and rename OS X Init.md to OS X Init.sh

--- OS X Init.md
+++
@@ -1,2 +0,0 @@
-Oceanic Theme for OSX Terminal [link](https://github.com/robinbentley/oceanic-next-macos-terminal)
-`git clone git@github.com:robinbentley/oceanic-next-macos-terminal.git`
---
+++ OS X Init.sh
@@ -0,0 +1,5 @@
+echo "Install Oceanic Theme for OSX Terminal from https://github.com/robinbentley/oceanic-next-macos-terminal"
+cd ~/Download/
+mkdir _tmp
+git clone git@github.com:robinbentley/oceanic-next-macos-terminal.git
+echo "Cloned. Plz import into Preference of Terminal"
