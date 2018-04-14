b2d4f1e103d640a2bfe4ddf7b3a29f23f059726b
bunny-girl <2567173338@qq.com>
2018-04-14 11:35:41 +0800

    Updated the tips

--- OS X Init.sh
+++ OS X Init.sh
@@ -1,4 +1,4 @@
-read -p "Install Homebrew? y/n\n" hb
+read -p "Install Homebrew? y/n " hb
 case $hb in
   "y")
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -8,8 +8,9 @@ case $hb in
     echo "Homebrew installation cancelled."
     ;;
 esac
+echo ""
 
-read -p "Install NodeJS? y/n\n" n
+read -p "Install NodeJS? y/n " n
 case $n in
   "y")
     brew install nodejs
@@ -19,21 +20,9 @@ case $n in
     echo "NodeJS installation cancelled."
     ;;
 esac
+echo ""
 
-read -p "Download Oceanic Theme for OSX Terminal\nFrom https://github.com/robinbentley/oceanic-next-macos-terminal ? y/n\n" ocean
-case $ocean in
-  "y")
-    cd ~/Download/
-    mkdir _tmp
-    git clone git@github.com:robinbentley/oceanic-next-macos-terminal.git
-    echo "Cloned. Plz import into Preference of Terminal"
-    ;;
-  "n")
-    echo "Download cancelled."
-    ;;
-esac
-
-read -p "Generate ssh keys? y/n\n" sshkey
+read -p "Generate ssh keys? y/n " sshkey
 case $sshkey in
   "y")
     ssh-keygen -t rsa 
@@ -42,3 +31,4 @@ case $sshkey in
     echo "SSH key generation cancelled"
     ;;
 esac
+echo ""
